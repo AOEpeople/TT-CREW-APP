a471f0d950ff0a3f2cfad12dacc8c6025330e2af
ansgar.hoyer <ansgar.hoyer@aoe.com>
2024-12-20 12:55:01 +0100

    feat(wip): update sst to version 3

--- sst.config.ts
+++ sst.config.ts
@@ -1,42 +1,28 @@
-import { SSTConfig } from "sst";
-import { Config, NextjsSite } from "sst/constructs";
+/// <reference path="./.sst/platform/config.d.ts" />
 
-export default {
-  config(_input) {
+export default $config({
+  app(input) {
     return {
       name: "aoe-tabletennis-app",
-      region: "eu-central-1",
+      removal: input?.stage === "production" ? "retain" : "remove",
+      protect: ["production"].includes(input?.stage),
+      home: "aws"
     };
   },
-  stacks(app) {
-    app.stack(function Site({ stack }) {
-      const DATABASE = new Config.Secret(stack, "DATABASE");
+  async run() {
+/**  const DATABASE = new Config.Secret(stack, "DATABASE");
       const DB_HOST = new Config.Secret(stack, "DB_HOST");
       const DB_USER = new Config.Secret(stack, "DB_USER");
       const DB_PASSWORD = new Config.Secret(stack, "DB_PASSWORD");
-      const SENTRY_AUTH_TOKEN = new Config.Secret(stack, "SENTRY_AUTH_TOKEN");
+      const SENTRY_AUTH_TOKEN = new Config.Secret(stack, "SENTRY_AUTH_TOKEN");*/
+    const DATABASE = new sst.Secret("DATABASE");
+    const DB_HOST = new sst.Secret("DB_HOST");
+    const DB_USER = new sst.Secret("DB_USER");
+    const DB_PASSWORD = new sst.Secret("DB_PASSWORD");
+    const SENTRY_AUTH_TOKEN = new sst.Secret("SENTRY_AUTH_TOKEN");
 
-      const site = new NextjsSite(stack, "site", {
-        customDomain: {
-          domainName:
-            stack.stage === "prod"
-              ? "tt-crew.app"
-              : `${stack.stage}.tt-crew.app`,
-          hostedZone: "tt-crew.app",
-        },
-        bind: [DATABASE, DB_HOST, DB_USER, DB_PASSWORD, SENTRY_AUTH_TOKEN],
-        environment: {
-          DATABASE: process.env.DATABASE,
-          DB_HOST: process.env.DB_HOST,
-          DB_USER: process.env.DB_USER,
-          DB_PASSWORD: process.env.DB_PASSWORD,
-          SENTRY_AUTH_TOKEN: process.env.SENTRY_AUTH_TOKEN,
-        },
-      });
-
-      stack.addOutputs({
-        SiteUrl: site.url,
-      });
+    new sst.aws.Nextjs("MyWeb", {
+      link: [DATABASE, DB_HOST, DB_USER, DB_PASSWORD, SENTRY_AUTH_TOKEN],
     });
   },
-} satisfies SSTConfig;
+});
