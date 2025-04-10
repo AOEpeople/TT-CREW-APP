05c2fa68479cb35ad673fb4d26b0385ab8e45594
Ansgar Hoyer <ansgar.hoyer@gmail.com>
2025-04-10 10:51:11 +0200

    feat: starting migrating to a monorepo setup

--- src/lib/formDataToObject.ts
+++
@@ -1,7 +0,0 @@
-export const formDataToObject = (formData: FormData) => {
-  const object: Record<string, unknown> = {};
-  formData.forEach((value, key) => {
-    object[key] = value;
-  });
-  return object;
-};
